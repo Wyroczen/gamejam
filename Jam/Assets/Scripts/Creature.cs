b3f9082d191be97ba799eb6f98ba80fd9038f90f
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-04-03 20:16:32 +0200

    attack

--- Jam/Assets/Scripts/Creature.cs
+++ Jam/Assets/Scripts/Creature.cs
@@ -32,7 +32,7 @@ namespace Assets.Scripts
         }
         public void Attack()
         {
-            Animator.SetTrigger("Attack");
+            Animator.SetTrigger(Constants.Attack);
         }
     }
 }
