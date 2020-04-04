b8003a84dc39f29e985e8a51c007d9d329822961
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-04-04 17:30:26 +0200

    Spawn monsters

---
+++ Jam/Assets/Scripts/SpawnMonster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SpawnMonster : MonoBehaviour
+    {
+        public GameObject Monster;
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                GameObject newMonster = Instantiate(Monster) as GameObject;
+                newMonster.transform.position = new Vector3(29.61842f, -1.434389f, -1.4f);
+            }
+        }
+    }
+}
