6eeda6d54edbfbf989538b099db8ca627cf754c1
Marianna Murawicz <marianna.murawicz@student.put.poznan.pl>
2020-04-05 19:52:22 +0200

    test

---
+++ Jam/Assets/Teleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private string nextSceneName;
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Elo");
+        if (collision.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(nextSceneName);
+    }
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
