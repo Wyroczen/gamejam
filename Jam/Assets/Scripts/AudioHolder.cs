3ef8c29365e07617552a0d2884d35414bc9146ae
TrumpSsta <jedrzeej97@gmail.com>
2020-04-05 18:50:54 +0200

    Created music added to scenes
    Background music works on all scenes

---
+++ Jam/Assets/Scripts/AudioHolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHolder : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
+
