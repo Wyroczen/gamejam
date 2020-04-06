17883ec8cf73a436ab8695d9befd7f77b66b6192
TrumpSsta <jedrzeej97@gmail.com>
2020-04-06 11:42:17 +0200

    Item value

--- Jam/Assets/Scripts/InventorySlot.cs
+++ Jam/Assets/Scripts/InventorySlot.cs
@@ -30,4 +30,12 @@ public class InventorySlot : MonoBehaviour
     {
         Inventory.instance.Remove(item);
     }
+
+    public void UseItem()
+    {
+        if(item != null)
+        {
+            item.Use();
+        }
+    }
 }
